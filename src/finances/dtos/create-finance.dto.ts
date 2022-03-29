e16440de65d4ac1a96d098a15b7f883872ec06d7
Matheus Vargem <matheusvrgm@gmail.com>
2022-03-28 22:44:22 -0300

    feat: json body validation

--- src/finances/dtos/create-finance.dto.ts
+++ src/finances/dtos/create-finance.dto.ts
@@ -1,9 +1,21 @@
+import { IsEnum, IsNotEmpty } from 'class-validator';
 import { FinanceCategory, FinanceType } from '../models/finance.model';
 
 export class CreateFinanceDTO {
+  @IsNotEmpty({ message: 'Name is required' })
   name: string;
+
+  @IsNotEmpty({ message: 'Category is required' })
+  @IsEnum(FinanceCategory)
   category: FinanceCategory;
+
+  @IsNotEmpty({ message: 'Type is required' })
+  @IsEnum(FinanceType)
   type: FinanceType;
+
+  @IsNotEmpty({ message: 'Number is required' })
   value: number;
+
+  @IsNotEmpty({ message: 'Date is required' })
   date: Date;
 }
