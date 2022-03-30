6f0957944dbe510c3130a6f0e1e52082719446c7
Matheus Vargem <matheusvrgm@gmail.com>
2022-03-29 22:03:00 -0300

    feat: environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FinancesModule } from './finances/finances.module';
 import { Transaction } from './finances/models/transaction.model';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      url: 'mongodb://localhost',
-      database: 'transactions',
+      url: process.env.MONGODB_URL,
+      database: process.env.MONGODB_DATABASE,
       synchronize: true,
       entities: [Transaction],
       useUnifiedTopology: true,
