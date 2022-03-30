024a119326df1e2abeb2e6d66b37394b438a2fce
Matheus Vargem <matheusvrgm@gmail.com>
2022-03-29 22:34:23 -0300

    feat: swagger

--- src/finances/controllers/transactions.controller.ts
+++ src/finances/controllers/transactions.controller.ts
@@ -9,6 +9,15 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateFinanceDTO } from '../dtos/create-finance.dto';
 import { FilterTransactionDTO } from '../dtos/filter-transaction.dto';
 import { SearchTransactionDTO } from '../dtos/search-transaction.dto';
@@ -19,29 +28,67 @@ import {
 } from '../models/transaction.model';
 import { TransactionsService } from '../services/transactions.service';
 
+@ApiTags('finances')
 @Controller('transactions')
 export class TransactionsController {
   constructor(private financesService: TransactionsService) {}
 
   @Get()
   @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Get a list of all transactions',
+  })
+  @ApiOperation({
+    description: 'Get a list of all transactions',
+    summary: 'Get a list of all transactions',
+  })
   public async getAllFinances(): Promise<Transaction[]> {
     return this.financesService.getAllTransactions();
   }
 
   @Get('/filter')
   @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Get a list of all transactions within a filter',
+  })
+  @ApiQuery({
+    name: 'type',
+    description: "The transaction's type",
+    enum: TransactionType,
+    example: TransactionType.EXPENSE,
+  })
+  @ApiQuery({
+    name: 'category',
+    description: "The transaction's category",
+    enum: TransactionCategory,
+    example: TransactionCategory.FOOD,
+  })
+  @ApiOperation({
+    description: 'Get a list of all transactions within a filter',
+    summary: 'Get a list of all transactions within a filter',
+  })
   public async getFilteredTransactions(
     @Query('type') type: TransactionType,
     @Query('category') category: TransactionCategory,
   ): Promise<Transaction[]> {
     const filter: FilterTransactionDTO = { category, type };
-    console.log(filter);
     return this.financesService.getFilteredTransactions(filter);
   }
 
   @Get('/search')
   @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Get a list of all transactions within a search keyword',
+  })
+  @ApiQuery({
+    name: 'keyword',
+    description: "A keyword to be search in transactions\\' value or name",
+    example: 'Subscription',
+  })
+  @ApiOperation({
+    description: 'Get a list of all transactions within a search keyword',
+    summary: 'Get a list of all transactions within a search keyword',
+  })
   public async searchTransactions(
     @Query('keyword') keyword: string,
   ): Promise<Transaction[]> {
@@ -51,12 +98,29 @@ export class TransactionsController {
 
   @Delete('/:id')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiNoContentResponse({
+    description: 'Deletes the transaction however does not return anything',
+  })
+  @ApiParam({
+    name: 'id',
+    description: "The transactions' id to be deleted",
+    example: '62435577aeb1e58f5a081d51',
+  })
+  @ApiOperation({
+    description: 'Deletes the transaction however does not return anything',
+    summary: 'Deletes the transaction however does not return anything',
+  })
   public async deleteFinance(@Param('id') objectId: string): Promise<void> {
     return this.financesService.deleteTransaction(objectId);
   }
 
   @Post()
   @HttpCode(201)
+  @ApiCreatedResponse({ description: 'Creates a new transaction' })
+  @ApiOperation({
+    description: 'Creates a new transaction',
+    summary: 'Creates a new transaction',
+  })
   public async createFinance(
     @Body() createFinanceDTO: CreateFinanceDTO,
   ): Promise<Transaction> {
--- src/finances/dtos/create-finance.dto.ts
+++ src/finances/dtos/create-finance.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty } from 'class-validator';
 import {
   TransactionCategory,
@@ -5,20 +6,41 @@ import {
 } from '../models/transaction.model';
 
 export class CreateFinanceDTO {
+  @ApiProperty({
+    example: 'Burger Queen Order',
+    description: 'The name of the transaction',
+  })
   @IsNotEmpty({ message: 'Name is required' })
   name: string;
 
-  @IsNotEmpty({ message: 'Category is required' })
+  @ApiProperty({
+    example: TransactionCategory.FOOD,
+    description: "The transactions' category",
+    enum: TransactionCategory,
+  })
+  @IsNotEmpty({
+    message: 'Category is required',
+  })
   @IsEnum(TransactionCategory)
   category: TransactionCategory;
 
+  @ApiProperty({
+    description: "The transactions' type",
+    example: TransactionType.EXPENSE,
+    enum: TransactionType,
+  })
   @IsNotEmpty({ message: 'Type is required' })
   @IsEnum(TransactionType)
   type: TransactionType;
 
+  @ApiProperty({
+    description: "The transactions' value",
+    example: 25.9,
+  })
   @IsNotEmpty({ message: 'Number is required' })
   value: number;
 
+  @ApiProperty({ description: "The transactions' date" })
   @IsNotEmpty({ message: 'Date is required' })
   date: Date;
 }
